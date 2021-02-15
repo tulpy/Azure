86fcf620b4a92ebcfe2d46f1e6a0616ec01a37ea
tulpy <stephen.tulp@outlook.com>
2021-02-15 09:45:42 +0800

    code clean up

--- AzureSecurityCenter/Powershell scripts/Subscription Management/AzASCSubCount.ps1
+++ AzureSecurityCenter/Powershell scripts/Subscription Management/AzASCSubCount.ps1
@@ -2,43 +2,43 @@
 
 $AzModule = Get-Module -Name "Az.*" -ListAvailable
 
-if ($AzModule -eq $null) {
+if ($null -eq $AzModule) {
 
-    Write-Verbose "Azure PowerShell module not found"
+        Write-Verbose "Azure PowerShell module not found"
 
-    #check for Admin Privleges
+        #check for Admin Privleges
 
-    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+        $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 
-    if(-not ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))){
+        if (-not ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
 
-        #No Admin, install to current user
+                #No Admin, install to current user
 
-        Write-Warning -Message "Can not install Az Module.  You are not running as Administrator"
+                Write-Warning -Message "Can not install Az Module.  You are not running as Administrator"
 
-        Write-Warning -Message "Installing Az Module to Current User Scope"
+                Write-Warning -Message "Installing Az Module to Current User Scope"
 
-        Install-Module Az -Scope CurrentUser -Force
+                Install-Module Az -Scope CurrentUser -Force
 
-        Install-Module Az.Security -Scope CurrentUser -Force
+                Install-Module Az.Security -Scope CurrentUser -Force
 
-        Install-Module Az.Accounts -Scope CurrentUser -Force
+                Install-Module Az.Accounts -Scope CurrentUser -Force
 
   
-    }
+        }
 
-    Else{
+        Else {
 
-        #Admin, install to all users
+                #Admin, install to all users
 
-        Install-Module -Name Az -AllowClobber -Force
+                Install-Module -Name Az -AllowClobber -Force
 
-        Import-Module -Name Az.Accounts -Force
+                Import-Module -Name Az.Accounts -Force
 
-        Import-Module -Name Az.Security -Force
+                Import-Module -Name Az.Security -Force
       
 
-    }
+        }
 
 }
 
@@ -54,85 +54,85 @@ $Subscriptions = Get-AzSubscription
 Write-Verbose "Searching each subscription can take up to 30 seconds each"
 Write-Host " Please wait while searching all" ($Subscriptions).Count "subscriptions"
 
-foreach($Subscription in $Subscriptions){
+foreach ($Subscription in $Subscriptions) {
 
-    $Id = ($Subscription.Id)
+        $Id = ($Subscription.Id)
 
-    Select-AzSubscription $Id | Out-Null
+        Select-AzSubscription $Id | Out-Null
 
-    #Get Security Task for App Services
+        #Get Security Task for App Services
 
-    $ASC += Get-AzSecurityPricing
+        $ASC += Get-AzSecurityPricing
 
 }
 
 #Total Subscriptions you've collected
 Write-Host " Here is a list of all the subscriptions you've just searched"
-$Subscriptions | ft
+$Subscriptions | Format-Table
 
 # Providing count of Free and Standard within Subscriptions:
 
-$FreeAppServices = $ASC | where{$_.PricingTier -eq "Free"} | where{$_.Name -eq "AppServices"}
-$FreeStorageAccounts = $ASC | where{$_.PricingTier -eq "Free"} | where{$_.Name -eq "StorageAccounts"}
-$FreeSQLServers = $ASC | where{$_.PricingTier -eq "Free"} | where{$_.Name -eq "SqlServers"}
-$FreeVirtualMachines = $ASC | where{$_.PricingTier -eq "Free"} | where{$_.Name -eq "VirtualMachines"}
-$StandardAppServices = $ASC | where{$_.PricingTier -eq "Standard"} | where{$_.Name -eq "AppServices"}
-$StandardStorageAccounts = $ASC | where{$_.PricingTier -eq "Standard"} | where{$_.Name -eq "StorageAccounts"}
-$StandardSQLServers = $ASC | where{$_.PricingTier -eq "Standard"} | where{$_.Name -eq "SqlServers"}
-$StandardVirtualMachines = $ASC | where{$_.PricingTier -eq "Standard"} | where{$_.Name -eq "VirtualMachines"}
+$FreeAppServices = $ASC | where-object { $_.PricingTier -eq "Free" } | where-object { $_.Name -eq "AppServices" }
+$FreeStorageAccounts = $ASC | where-object { $_.PricingTier -eq "Free" } | where-object { $_.Name -eq "StorageAccounts" }
+$FreeSQLServers = $ASC | where-object { $_.PricingTier -eq "Free" } | where-object { $_.Name -eq "SqlServers" }
+$FreeVirtualMachines = $ASC | where-object { $_.PricingTier -eq "Free" } | where-object { $_.Name -eq "VirtualMachines" }
+$StandardAppServices = $ASC | where-object { $_.PricingTier -eq "Standard" } | where-object { $_.Name -eq "AppServices" }
+$StandardStorageAccounts = $ASC | where-object { $_.PricingTier -eq "Standard" } | where-object { $_.Name -eq "StorageAccounts" }
+$StandardSQLServers = $ASC | where-object { $_.PricingTier -eq "Standard" } | where-object { $_.Name -eq "SqlServers" }
+$StandardVirtualMachines = $ASC | where-object { $_.PricingTier -eq "Standard" } | where-object { $_.Name -eq "VirtualMachines" }
 
 Write-Host " The Total Count of Azure Security Center 'Free' subscriptions " -NoNewline
-Write-Host ($ASC | where{$_.PricingTier -eq "Free"} | Measure-Object).Count -ForegroundColor Red
+Write-Host ($ASC | where-object { $_.PricingTier -eq "Free" } | Measure-Object).Count -ForegroundColor Red
 Write-Host " Free Sub - App Services " -NoNewline
-Write-Host ($ASC | where{$_.PricingTier -eq "Free"} | where{$_.Name -eq "AppServices"} | Measure-Object).Count -ForegroundColor Red
+Write-Host ($ASC | where-object { $_.PricingTier -eq "Free" } | where-object { $_.Name -eq "AppServices" } | Measure-Object).Count -ForegroundColor Red
 Write-Host " Free Sub - Storage Accounts " -NoNewline
-Write-Host ($ASC | where{$_.PricingTier -eq "Free"} | where{$_.Name -eq "StorageAccounts"} | Measure-Object).Count -ForegroundColor Red
+Write-Host ($ASC | where-object { $_.PricingTier -eq "Free" } | where-object { $_.Name -eq "StorageAccounts" } | Measure-Object).Count -ForegroundColor Red
 Write-Host " Free Sub - Sql Servers " -NoNewline
-Write-Host ($ASC | where{$_.PricingTier -eq "Free"} | where{$_.Name -eq "SqlServers"} | Measure-Object).Count -ForegroundColor Red
+Write-Host ($ASC | where-object { $_.PricingTier -eq "Free" } | where-object { $_.Name -eq "SqlServers" } | Measure-Object).Count -ForegroundColor Red
 Write-Host " Free Sub - Virtual Machines " -NoNewline
-Write-Host ($ASC | where{$_.PricingTier -eq "Free"} | where{$_.Name -eq "VirtualMachines"} | Measure-Object).Count -ForegroundColor Red
-foreach($FreeVirtualMachine in $FreeVirtualMachines){
+Write-Host ($ASC | where-object { $_.PricingTier -eq "Free" } | where-object { $_.Name -eq "VirtualMachines" } | Measure-Object).Count -ForegroundColor Red
+foreach ($FreeVirtualMachine in $FreeVirtualMachines) {
 
-    $Id = ($FreeVirtualMachine.Id.Split("/")[2])
+        $Id = ($FreeVirtualMachine.Id.Split("/")[2])
 
-    Select-AzSubscription $Id | Out-Null
+        Select-AzSubscription $Id | Out-Null
 
-    #Get Security Task for App Services
+        #Get Security Task for App Services
 
-    $FreeVMCount += Get-AzVM
+        $FreeVMCount += Get-AzVM
 
 }
 Write-Host " Total Node Count " -NoNewline
 Write-Host ($FreeVMCount | Measure-Object).Count -ForegroundColor Yellow
 Write-Host " Total Provisioned " -NoNewline
-Write-Host ($FreeVMCount | where {$_.ProvisioningState -eq "Succeeded"}).count -ForegroundColor Green
+Write-Host ($FreeVMCount | where-object { $_.ProvisioningState -eq "Succeeded" }).count -ForegroundColor Green
 Write-Host " Total Unprovisioned " -NoNewline
-Write-Host ($FreeVMCount | where {$_.ProvisioningState -eq "Failed"}).count -ForegroundColor Red
+Write-Host ($FreeVMCount | where-object { $_.ProvisioningState -eq "Failed" }).count -ForegroundColor Red
 
 Write-Host " The Total Count of Azure Security Center 'Standard' subscriptions " -NoNewline
-Write-Host ($ASC | where{$_.PricingTier -eq "Standard"} | Measure-Object).Count -ForegroundColor Green
+Write-Host ($ASC | where-object { $_.PricingTier -eq "Standard" } | Measure-Object).Count -ForegroundColor Green
 Write-Host " Standard Sub - App Services " -NoNewline
-Write-Host ($ASC | where{$_.PricingTier -eq "Standard"} | where{$_.Name -eq "AppServices"} | Measure-Object).Count -ForegroundColor Green
+Write-Host ($ASC | where-object { $_.PricingTier -eq "Standard" } | where-object { $_.Name -eq "AppServices" } | Measure-Object).Count -ForegroundColor Green
 Write-Host " Standard Sub - Storage Accounts " -NoNewline
-Write-Host ($ASC | where{$_.PricingTier -eq "Standard"} | where{$_.Name -eq "StorageAccounts"} | Measure-Object).Count -ForegroundColor Green
+Write-Host ($ASC | where-object { $_.PricingTier -eq "Standard" } | where-object { $_.Name -eq "StorageAccounts" } | Measure-Object).Count -ForegroundColor Green
 Write-Host " Standard Sub - Sql Servers " -NoNewline
-Write-Host ($ASC | where{$_.PricingTier -eq "Standard"} | where{$_.Name -eq "SqlServers"} | Measure-Object).Count -ForegroundColor Green
+Write-Host ($ASC | where-object { $_.PricingTier -eq "Standard" } | where-object { $_.Name -eq "SqlServers" } | Measure-Object).Count -ForegroundColor Green
 Write-Host " Standard Sub - Virtual Machines "-NoNewline
-Write-Host ($ASC | where{$_.PricingTier -eq "Standard"} | where{$_.Name -eq "VirtualMachines"} | Measure-Object).Count -ForegroundColor Green
-foreach($StandardVirtualMachine in $StandardVirtualMachines){
+Write-Host ($ASC | where-object { $_.PricingTier -eq "Standard" } | where-object { $_.Name -eq "VirtualMachines" } | Measure-Object).Count -ForegroundColor Green
+foreach ($StandardVirtualMachine in $StandardVirtualMachines) {
 
-    $Id = ($StandardVirtualMachine.Id.Split("/")[2])
+        $Id = ($StandardVirtualMachine.Id.Split("/")[2])
 
-    Select-AzSubscription $Id | Out-Null
+        Select-AzSubscription $Id | Out-Null
 
-    #Get Security Task for App Services
+        #Get Security Task for App Services
 
-    $StandardVMCount += Get-AzVM
+        $StandardVMCount += Get-AzVM
 
 }
 Write-Host " Total Node Count " -NoNewline
 Write-Host ($StandardVMCount | Measure-Object).Count -ForegroundColor Yellow
 Write-Host " Total Provisioned " -NoNewline
-Write-Host ($StandardVMCount | where {$_.ProvisioningState -eq "Succeeded"}).count -ForegroundColor Green
+Write-Host ($StandardVMCount | where-object { $_.ProvisioningState -eq "Succeeded" }).count -ForegroundColor Green
 Write-Host " Total Unprovisioned " -NoNewline
-Write-Host ($StandardVMCount | where {$_.ProvisioningState -eq "Failed"}).count -ForegroundColor Red
+Write-Host ($StandardVMCount | where-object { $_.ProvisioningState -eq "Failed" }).count -ForegroundColor Red
